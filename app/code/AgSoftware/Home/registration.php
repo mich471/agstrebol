2c5d02e9c4c3215761dada50f0e575eff3c5725c
angel coyasos <angelcotr@gmail.com>
2023-05-17 12:05:43 -0500

    config: add files upgrades

--- app/code/AgSoftware/Home/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © none All rights reserved.
- * See COPYING.txt for license details.
- */
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AgSoftware_Home', __DIR__);
-
